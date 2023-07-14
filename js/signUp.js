4547317e9da86f6bb8420dd5486c840ebb669b66
zeinab <zshkh2000@gmail.com>
2023-07-14 22:12:05 +0330

    adding require images

---
+++ js/signUp.js
@@ -0,0 +1,15 @@
+/* const namee=document.querySelector("#name")
+const email=document.querySelector("#email")
+const telephone=document.querySelector("#telephone")
+const teacher=document.querySelector("#teacher")
+const choiceCourse=document.querySelector("#choiceCourse")
+const registerBox=document.querySelector(".registerBox")
+const flexCheckDefault=document.querySelector("#flexCheckDefault")
+const son='{}'
+registerBox.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const result=`${namee.value} ${email.value} ${telephone.value} ${teacher.value} ${choiceCourse.value} ${flexCheckDefault.value}`
+    const son=JASON.stringify(result)
+    console.log(son)
+
+ }) */
